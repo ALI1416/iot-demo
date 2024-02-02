519b3274db2793f17efcdf559695b1c354339381
ALI <1416978277@qq.com>
2024-02-02 11:23:18 +0800

    0202

---
+++ iot-web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
