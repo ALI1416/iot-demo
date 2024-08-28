d0ec268e6f60c1c9f247a07d88c7645b3adbabd8
ALI <1416978277@qq.com>
2024-08-28 17:53:27 +0800

    0828

--- iot-service-single/iot-service-single-api/src/main/java/com/demo/service/EventReportService.java
+++ iot-service-single/iot-service-single-api/src/main/java/com/demo/service/EventReportService.java
@@ -6,6 +6,7 @@ import com.demo.base.ServiceBase;
 import com.demo.constant.ReportType;
 import com.demo.dao.mongo.EventDao;
 import com.demo.entity.protocol.Protocol;
+import com.demo.util.DateUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -41,15 +42,31 @@ public class EventReportService extends ServiceBase {
     @Async
     @Scheduled(cron = "11 * * * * *")
     public void reportMinute() {
-        // 这一分钟的0秒0毫秒
-        long timestampEnd = TimestampUtils.getTimestamp(Clock.now(), Calendar.SECOND, true, -1, 0);
-        // 上一分钟的0秒0毫秒
-        long timestampStart = timestampEnd - TimestampUtils.MILLS_OF_MINUTE;
-        Calendar calendar = new Builder().setInstant(timestampStart).build();
+        // 上一分钟
+        Calendar calendar = new Builder().setInstant(Clock.now() - TimestampUtils.MILLS_OF_MINUTE).build();
         int year = calendar.get(YEAR);
         int month = calendar.get(MONTH) + 1;
+        int day = calendar.get(DAY_OF_MONTH);
+        int hour = calendar.get(HOUR_OF_DAY);
+        int minute = calendar.get(MINUTE);
+        reportMinute(year, month, day, hour, minute);
+    }
+
+    /**
+     * 分钟报表
+     *
+     * @param year   年
+     * @param month  月
+     * @param day    日
+     * @param hour   小时
+     * @param minute 分钟
+     */
+    public void reportMinute(int year, int month, int day, int hour, int minute) {
+        // 这一分钟的0秒0毫秒
+        long timestampStart = DateUtils.getTimestamp(year, month, day, hour, minute, 0, 0);
         Timestamp createTimeStart = new Timestamp(timestampStart);
-        Timestamp createTimeEnd = new Timestamp(timestampEnd);
+        // 下一分钟的0秒0毫秒
+        Timestamp createTimeEnd = new Timestamp(timestampStart + TimestampUtils.MILLS_OF_MINUTE);
         for (int gatewaySn : GatewayService.getGatewaySnList()) {
             // 全部数据
             eventDao.find(gatewaySn, year, month, createTimeStart, createTimeEnd)
@@ -61,6 +78,56 @@ public class EventReportService extends ServiceBase {
                                     eventDao.insertReport(Protocol.getEventReportMinute(list), gatewaySn, year, ReportType.MINUTE)
                             )
                     );
+        }
+    }
+
+    /**
+     * 分钟报表
+     *
+     * @param year  年
+     * @param month 月
+     * @param day   日
+     * @param hour  小时
+     */
+    public void reportMinute(int year, int month, int day, int hour) {
+        for (int i = 0; i < 60; i++) {
+            reportMinute(year, month, day, hour, i);
+        }
+    }
+
+    /**
+     * 分钟报表
+     *
+     * @param year  年
+     * @param month 月
+     * @param day   日
+     */
+    public void reportMinute(int year, int month, int day) {
+        for (int i = 0; i < 24; i++) {
+            reportMinute(year, month, day, i);
+        }
+    }
+
+    /**
+     * 分钟报表
+     *
+     * @param year  年
+     * @param month 月
+     */
+    public void reportMinute(int year, int month) {
+        for (int i = 1; i < 32; i++) {
+            reportMinute(year, month, i);
+        }
+    }
+
+    /**
+     * 分钟报表
+     *
+     * @param year 年
+     */
+    public void reportMinute(int year) {
+        for (int i = 1; i < 13; i++) {
+            reportMinute(year, i);
         }
     }
 
@@ -77,6 +144,18 @@ public class EventReportService extends ServiceBase {
         int month = calendar.get(MONTH) + 1;
         int day = calendar.get(DAY_OF_MONTH);
         int hour = calendar.get(HOUR_OF_DAY);
+        reportHour(year, month, day, hour);
+    }
+
+    /**
+     * 小时报表
+     *
+     * @param year  年
+     * @param month 月
+     * @param day   日
+     * @param hour  小时
+     */
+    public void reportHour(int year, int month, int day, int hour) {
         for (int gatewaySn : GatewayService.getGatewaySnList()) {
             // 分钟报表
             eventDao.findReport(gatewaySn, year, month, day, hour, null, ReportType.MINUTE)
@@ -92,6 +171,42 @@ public class EventReportService extends ServiceBase {
     }
 
     /**
+     * 小时报表
+     *
+     * @param year  年
+     * @param month 月
+     * @param day   日
+     */
+    public void reportHour(int year, int month, int day) {
+        for (int i = 0; i < 24; i++) {
+            reportHour(year, month, day, i);
+        }
+    }
+
+    /**
+     * 小时报表
+     *
+     * @param year  年
+     * @param month 月
+     */
+    public void reportHour(int year, int month) {
+        for (int i = 1; i < 32; i++) {
+            reportHour(year, month, i);
+        }
+    }
+
+    /**
+     * 小时报表
+     *
+     * @param year 年
+     */
+    public void reportHour(int year) {
+        for (int i = 1; i < 13; i++) {
+            reportHour(year, i);
+        }
+    }
+
+    /**
      * 上一天 日报表(每天的0时2分21秒)
      */
     @Async
@@ -103,6 +218,17 @@ public class EventReportService extends ServiceBase {
         int year = calendar.get(YEAR);
         int month = calendar.get(MONTH) + 1;
         int day = calendar.get(DAY_OF_MONTH);
+        reportDay(year, month, day);
+    }
+
+    /**
+     * 日报表
+     *
+     * @param year  年
+     * @param month 月
+     * @param day   日
+     */
+    public void reportDay(int year, int month, int day) {
         for (int gatewaySn : GatewayService.getGatewaySnList()) {
             // 小时报表
             eventDao.findReport(gatewaySn, year, month, day, null, null, ReportType.HOUR)
@@ -118,6 +244,29 @@ public class EventReportService extends ServiceBase {
     }
 
     /**
+     * 日报表
+     *
+     * @param year  年
+     * @param month 月
+     */
+    public void reportDay(int year, int month) {
+        for (int i = 1; i < 32; i++) {
+            reportDay(year, month, i);
+        }
+    }
+
+    /**
+     * 日报表
+     *
+     * @param year 年
+     */
+    public void reportDay(int year) {
+        for (int i = 1; i < 13; i++) {
+            reportDay(year, i);
+        }
+    }
+
+    /**
      * 上一月 月报表(每月的1日0时3分26秒)
      */
     @Async
@@ -129,6 +278,16 @@ public class EventReportService extends ServiceBase {
         calendar.add(MONTH, -1);
         int year = calendar.get(YEAR);
         int month = calendar.get(MONTH) + 1;
+        reportMonth(year, month);
+    }
+
+    /**
+     * 月报表
+     *
+     * @param year  年
+     * @param month 月
+     */
+    public void reportMonth(int year, int month) {
         for (int gatewaySn : GatewayService.getGatewaySnList()) {
             // 日报表
             eventDao.findReport(gatewaySn, year, month, null, null, null, ReportType.DAY)
@@ -140,6 +299,17 @@ public class EventReportService extends ServiceBase {
                                     eventDao.insertReport(Protocol.getEventReportMonth(list), gatewaySn, year, ReportType.MONTH)
                             )
                     );
+        }
+    }
+
+    /**
+     * 月报表
+     *
+     * @param year 年
+     */
+    public void reportMonth(int year) {
+        for (int i = 1; i < 13; i++) {
+            reportMonth(year, i);
         }
     }
 
